31272bd917219c59bfa5cb81132d399611c19308
lokesh <lokeshvarma4511@gmail.com>
2025-03-24 16:38:32 +0000

    nitial

--- ClassObject.java
+++ ClassObject.java
@@ -1,13 +1,12 @@
 
 class Claculator
 {
+    int apple;
     int add(int a1,int b1)
     {
         return a1+b1;
     }
 }
-
-
 public class ClassObject {
     public static void main(String[] args) {
         Claculator c1=new Claculator();
