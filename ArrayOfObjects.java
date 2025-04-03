bbb9414c4ed6ca1cb3b8b84bda9fc23d075b6052
lokesh <lokeshvarma4511@gmail.com>
2025-04-03 16:26:38 +0100

    Added code for, Enhances FOR loop

--- ArrayOfObjects.java
+++ ArrayOfObjects.java
@@ -1,4 +1,3 @@
-
 class Student
 {
     int rollno;
@@ -18,20 +17,30 @@ public class ArrayOfObjects
         s1.Section='C';
 
         Student s2 = new Student();
-        s1.name="Raju, open tap";
-        s1.rollno=2;
-        s1.Blood="O-";
-        s1.Section='C';
-
+        s2.name="Raju, open tap";
+        s2.rollno=2;
+        s2.Blood="O-";
+        s2.Section='C';
         Student s3 = new Student();
-        s1.name="Chintu model, Kothapet";
-        s1.rollno=3;
-        s1.Blood="O-";
-        s1.Section='C';
+        s3.name="Chintu model, Kothapet";
+        s3.rollno=3;
+        s3.Blood="O-";
+        s3.Section='C';
 
         students[0]=s1;
         students[1]=s2;
         students[2]=s3;
         System.out.println(s1);
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i].name);
+        }
+
+        // ENHANCED FOR LOOP
+        System.out.println("----ENHANCED FOR LOOP----");
+        for(Student stud:students)
+        {
+            System.out.println(stud.name);
+        }
     }
 }
